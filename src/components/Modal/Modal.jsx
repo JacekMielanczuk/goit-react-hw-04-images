dfaa67e6295166bf925a5a82a0486aa325334705
JacekMielanczuk <mielanczuk1988@gmail.com>
2023-05-29 19:18:28 +0200

    refactor modala
    refactor modala

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Modal.module.css';
 import PropTypes from 'prop-types';
 
-class Modal extends React.Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.props.onKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.props.onKeyDown);
-  }
+// class Modal extends React.Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.props.onKeyDown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.props.onKeyDown);
+//   }
 
-  render() {
-    const { onBackdrop, children } = this.props;
+const Modal = ({ onBackdrop, onKeyDown, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
 
-    return (
-      <div className={styles.overlay} onClick={onBackdrop}>
-        <div className={styles.modal}>
-          <img src={children} alt={children} />
-        </div>
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onKeyDown]);
+
+  // render() {
+  //   const { onBackdrop, children } = this.props;
+
+  //   return (
+  //     <div className={styles.overlay} onClick={onBackdrop}>
+  //       <div className={styles.modal}>
+  //         <img src={children} alt={children} />
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <div className={styles.overlay} onClick={onBackdrop}>
+      <div className={styles.modal}>
+        <img src={children} alt={children} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   onKeyDown: PropTypes.func.isRequired,
